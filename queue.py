57c3cf63b5fe32bf62b0b848d024929630d793f4
Aniket-23 <117341962+Aniket-23@users.noreply.github.com>
2023-10-07 00:43:13 +0530

    added enqueue function
    to add new node to the queue

--- queue.py
+++ queue.py
@@ -8,14 +8,27 @@ class Queue:
         newNode = Node(value)
         self.first = newNode
         self.last = newNode
+        self.length = 1
 
     def printQueue(self) -> None:
         temp = self.first
-        if temp:
+        while temp:
             print(temp.value)
             temp = temp.next
+
+    def enqueue(self, value) -> None:
+        newNode = Node(value)
+        if self.first is None:
+            self.first = newNode
+            self.last = newNode
+        else:
+            self.last.next = newNode
+            self.last = newNode
+        self.length += 1
 
 
 
 queue = Queue(10)
+queue.enqueue(20)
+queue.enqueue(30)
 queue.printQueue()
