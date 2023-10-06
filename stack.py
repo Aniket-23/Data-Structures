10bc9bb8c3cee7d5c7b73f18c14d73e399ae9b28
Aniket-23 <117341962+Aniket-23@users.noreply.github.com>
2023-10-06 08:34:18 +0530

    added push function
    to push a node onto the stack

--- stack.py
+++ stack.py
@@ -7,7 +7,6 @@ class Stack:
     def __init__(self, value) -> None:
         newNode = Node(value)
         self.top = newNode
-        self.bottom = newNode
         self.height = 1
 
     def printStack(self) -> None:
@@ -16,7 +15,18 @@ class Stack:
             print(temp.value)
             temp = temp.next
 
+    def push(self, value) -> None:
+        newNode = Node(value)
+        if self.height == 0:
+            self.top = newNode
+        else:
+            newNode.next = self.top
+            self.top = newNode
+        self.height += 1
+
 
 
 stack  = Stack(10)
+stack.push(5)
+stack.push(1)
 stack.printStack()
