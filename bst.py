7ff4b384bbdf8c4a55cf8bde5d1c40b7010699de
Aniket-23 <117341962+Aniket-23@users.noreply.github.com>
2023-10-12 13:56:23 +0530

    added insert
    added insert function to binary search tree

--- bst.py
+++ bst.py
@@ -8,4 +8,31 @@ class BinarySearchTree:
     def __init__(self) -> None:
         self.root = None
 
+    def insert(self, value) -> bool:
+        newNode = Node(value=value)
+        if self.root is None:
+            self.root = newNode
+            return True
+        temp = self.root
+        while True:
+            if newNode.value == temp.value:
+                return False
+            if newNode.value < temp.value:
+                if temp.left is None:
+                    temp.left = newNode
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = newNode
+                    return True
+                temp = temp.right
+
 tree = BinarySearchTree()
+tree.insert(2)
+tree.insert(1)
+tree.insert(3)
+
+print(f"Root: {tree.root.value}")
+print(f"Left node: {tree.root.left.value}")
+print(f"Right node: {tree.root.right.value}")
